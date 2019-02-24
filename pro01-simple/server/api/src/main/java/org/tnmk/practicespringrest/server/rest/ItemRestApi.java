e2071e12fd725602ee5b73ffe7e15b4d23ec441f
Khoi (Kevin) Tran <khoi.tnm@gmail.com>
2019-02-24 12:41:56 -0500

    Create `pro01-simple.server.api` module.

---
+++ pro01-simple/server/api/src/main/java/org/tnmk/practicespringrest/server/rest/ItemRestApi.java
@@ -0,0 +1,7 @@
+package org.tnmk.practicespringrest.server.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(ApiConstants.API_BASE)
+public class ItemRestApi {
+}
