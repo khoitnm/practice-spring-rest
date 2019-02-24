bda10b11f4065a303669acb1ede14818f19fc572
Khoi (Kevin) Tran <khoi.tnm@gmail.com>
2019-02-23 20:23:30 -0500

    Init project structure with basic Spring configuration.

--- sample-grpc-server-02-error-and-header/README.md
+++
@@ -1,1 +0,0 @@
-Ref: https://github.com/LogNet/grpc-spring-boot-starter 
--- sample-grpc-server-02-error-and-header/src/main/java/org/tnmk/common/grpc/global/MDCConstants.java
+++
@@ -1,5 +0,0 @@
-package org.tnmk.common.grpc.global;
-
-public class MDCConstants {
-    public static final String CORRELATION_ID = "correlationId";
-}
